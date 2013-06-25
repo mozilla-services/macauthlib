ae5d409e44fe3542e5468b88242fd1a6c2d67697
Ryan Kelly <rkelly@mozilla.com>
2013-06-25 15:37:12 +1000

    Bump dev version number

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requires = ['webob']
 tests_requires = requires + ['requests']
 
 setup(name='macauthlib',
-      version='0.6.0',
+      version='0.6.1.dev1',
       description='macauth',
       long_description=README + '\n\n' + CHANGES,
       license='MPLv2.0',
